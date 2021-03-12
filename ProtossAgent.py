fbcaff672d236fccdb82387d18d60eab1f75f60f
mehdibalbali <mehdibalbali@yahoo.fr>
2021-03-12 05:50:35 +0100

    add some changes

--- ProtossAgent.py
+++ ProtossAgent.py
@@ -1,10 +1,9 @@
+import random
+import numpy as np
+from absl import app
 from pysc2.agents import base_agent
 from pysc2.lib import actions, features, units
 from pysc2.env import sc2_env, run_loop
-from absl import app
-import random
-import numpy as np
-
 
 #to create a simple agent 
 class RawAgent(base_agent.BaseAgent):
@@ -18,11 +17,75 @@ def __init__(self):
     super(RawAgent, self).__init__()
     self.base_top_left = None
 
+
+def get_distances(self, obs, units, xy):
+    units_xy = [(unit.x, unit.y) for unit in units]
+    return np.linalg.norm(np.array(units_xy) - np.array(xy), axis=1)
+
 #utility method to help us to find raw units we own
 def get_my_units_by_type(self, obs, unit_type):
     return [unit for unit in obs.observation.raw_units
             if unit.unit_type == unit_type 
             and unit.alliance == features.PlayerRelative.SELF]
+
+def get_my_completed_units_by_type(self, obs, unit_type):
+    return [unit for unit in obs.observation.raw_units
+            if unit.unit_type == unit_type 
+            and unit.build_progress == 100
+            and unit.alliance == features.PlayerRelative.SELF]
+
+#step() method lets determine the base position based on the location of our Nexus
+def step(self, obs):
+    super(RawAgent, self).step(obs)
+  
+    if obs.first():
+      nexus = self.get_my_units_by_type(obs, units.Protoss.Nexus)[0]
+      self.base_top_left = (nexus.x < 32)
+     #to create a Pylon 
+    pylons = self.get_my_units_by_type(obs,units.Protoss.Pylon)
+    completed_pylons = self.get_my_completed_units_by_type(obs, units.Protoss.Pylon)
+    
+    # to create Gateways
+    gateways = self.get_my_units_by_type(obs, units.Protoss.Gateway)
+    completed_gateways = self.get_my_completed_units_by_type(obs, units.Protoss.Gateway)
+    
+    free_supply = (obs.observation.player.food_cap - obs.observation.player.food_used)
+
+    zealots = self.get_my_units_by_type(obs, units.Protoss.Zealot)
+
+    if len(pylons) == 0 and obs.observation.players.minerals >= 100:
+        probes = self.get_my_units_by_type(obs,units.Protoss.Probe) 
+        if len(probes) > 0:
+            pylon_xy = (22,20) if self.base_top_left else (35,42)
+            distances = self.get_distances(obs,probes,pylon_xy)
+            probe = probes[np.argmin(distances)]    # extract the probe with the shortest distance 
+            return actions.RAW_FUNCTIONS.Build_Pylon_pt("now",probe.tag, pylon_xy) # tell the probe to build the pylon 
+    
+    if (len(completed_pylons) > 0 and len(gateways) == 0 and 
+        obs.observation.player.minerals >= 150):
+      probes = self.get_my_units_by_type(obs, units.Protoss.Probe)
+      if len(probes) > 0:        
+        gateway_xy = (22, 24) if self.base_top_left else (35, 45)
+        distances = self.get_distances(obs, probes, gateway_xy)
+        probe = probes[np.argmin(distances)]
+        return actions.RAW_FUNCTIONS.Build_Gateway_pt("now", probe.tag, gateway_xy)
+    
+    if (len(completed_gateways) > 0 and obs.observation.player.minerals >= 100  and free_supply >= 2):    
+        gateway = gateways[0]
+        if gateway.order_length < 5:
+            return actions.RAW_FUNCTIONS.Train_Zealot_quick("now", gateway.tag)
+      
+    if free_supply < 2 and len(zealots) > 0:
+      attack_xy = (38, 44) if self.base_top_left else (19, 23)
+      distances = self.get_distances(obs, zealots, attack_xy)
+      zealot = zealots[np.argmax(distances)]
+      x_offset = random.randint(-4, 4)
+      y_offset = random.randint(-4, 4)
+      return actions.RAW_FUNCTIONS.Attack_pt(
+          "now", zealot.tag, (attack_xy[0] + x_offset, attack_xy[1] + y_offset))
+
+    return actions.RAW_FUNCTIONS.no_op()
+
 
 
 def main(unused_argv):
